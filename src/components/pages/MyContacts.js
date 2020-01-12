4a49ec91889ab02396260f7d36d954ca509cadae
Pierce Morrill <piercemorrill91@gmail.com>
2020-01-12 15:29:53 -0700

    starting dummy data, cleaned up contact Card

--- src/components/pages/MyContacts.js
+++ src/components/pages/MyContacts.js
@@ -9,11 +9,14 @@ import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -39,22 +42,50 @@ const useStyles = makeStyles(theme => ({
 export default function MyContacts() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [ anchorEl, setAnchorEl ] = React.useState(null);
+
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
 
   return (
     <Card className={classes.card}>
 
 {/* Card Front */}
       <CardHeader
-        
+
+        //Burger Menu = issue: burger menu doesnt close unless hit Esc button.
         action={
-          <IconButton aria-label="settings">
+          <IconButton aria-label="settings" onClick={handleClick}>
+            {/* <MoreVertIcon /> */}
             <MoreVertIcon />
+
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+
+              <MenuItem onClick={handleClose}>Copy</MenuItem>
+              <MenuItem onClick={handleClose}>Edit</MenuItem>
+              <MenuItem onClick={handleClose}>Delete</MenuItem>
+            </Menu>
+
           </IconButton>
+          
         }
+        
         title="NAME HERE"
         subheader="PHONE NUMBER"
       />
@@ -92,6 +123,7 @@ export default function MyContacts() {
         </IconButton>
 
       </CardActions>
+
 {/* This is the beginning of the dropdown */}
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
